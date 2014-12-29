363be4c8aadff7b03b7526e5a042573d418cdeac
asakeron <asakeron@gmail.com>
2014-12-29 20:53:07 -0200

    Display list of exercises as a vty-ui's List widget.

--- src/Engager.hs
+++ src/Engager.hs
@@ -1,10 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Engager where
 
-data Exercise = Exercise { exerciseTitle     :: String
-                         , exerciseStatement :: String
-                         , exerciseVerifier  :: String -> Either [String] String }
+import Graphics.Vty.Widgets.All
+import Control.Monad (forM_)
+import Data.Text
+
+data Exercise = Exercise { exerciseTitle     :: Text
+                         , exerciseStatement :: Text
+                         , exerciseVerifier  :: Text -> Either [Text] Text }
 
 run :: [Exercise] -> IO ()
-run []     = putStr ""
-run (x:xs) = do putStrLn (exerciseTitle x)
-                run xs
+run exercises = do exerciseList <- newList 1
+                   forM_ exercises (\e -> addToList exerciseList (exerciseTitle e) =<< plainText (exerciseTitle e))
+                   ui <- centered exerciseList
+                   fg <- newFocusGroup
+                   collection <- newCollection
+                   _ <- addToCollection collection ui fg 
+                   _ <- addToFocusGroup fg exerciseList
+                   runUi collection defaultContext
