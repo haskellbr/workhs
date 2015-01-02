32247e26ec4654630e2b6ac160de156ecc4513c5
asakeron <asakeron@gmail.com>
2015-01-01 23:27:30 -0200

    Exercise screen + shortcuts.

--- src/Engager/Core.hs
+++ src/Engager/Core.hs
@@ -1,10 +1,10 @@
 module Engager.Core where
 
 import Engager.Exercise               (Exercise)
-import Engager.Interface              (exerciseList)
+import Engager.Interface              (mainScreen)
 import Graphics.Vty.Widgets.Core      (defaultContext)
 import Graphics.Vty.Widgets.EventLoop (runUi)
 
 run :: [Exercise] -> IO ()
-run exercises = do mainScreen <- exerciseList exercises
-                   runUi mainScreen defaultContext
+run exercises = do screen <- mainScreen exercises
+                   runUi screen defaultContext
--- src/Engager/Interface.hs
+++ src/Engager/Interface.hs
@@ -1,22 +1,44 @@
-module Engager.Interface (exerciseList) where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Engager.Interface (mainScreen) where
 
 import Engager.Exercise               
+import Control.Monad                  (forM_)
 import Graphics.Vty.Input             (Key(..), Modifier(..))
-import Graphics.Vty.Widgets.Centering (centered)
+import Graphics.Vty.Widgets.Box       (vBox, setBoxChildSizePolicy, ChildSizePolicy(..))
 import Graphics.Vty.Widgets.Core      (newFocusGroup, addToFocusGroup, onKeyPressed)
 import Graphics.Vty.Widgets.EventLoop (Collection, newCollection, addToCollection)
-import Graphics.Vty.Widgets.List      (newTextList)
+import Graphics.Vty.Widgets.Text      (plainText)
+import Graphics.Vty.Widgets.List      (ActivateItemEvent(..), newList, addToList, onItemActivated)
 import System.Exit                    (exitSuccess)
 
-exerciseList :: [Exercise] -> IO Collection
-exerciseList exercises = do 
-                           list <- newTextList (map exerciseTitle exercises) 1
-                           ui <- centered list
-                           fg <- newFocusGroup
-                           fg `onKeyPressed` \_ key modifiers -> if key == KChar 'q' && MCtrl `elem` modifiers
-                                                                  then exitSuccess
-                                                                  else return False
-                           collection <- newCollection
-                           _ <- addToCollection collection ui fg 
-                           _ <- addToFocusGroup fg list
-                           return collection
+mainScreen :: [Exercise] -> IO Collection
+mainScreen exercises = do 
+                    title <- plainText "tutorial title"
+                    list  <- newList 1
+                    ui    <- vBox title list
+                    _     <- setBoxChildSizePolicy ui $ Percentage 10
+                    fg    <- newFocusGroup
+                    exitOnCtrlQ fg
+                    _     <- addToFocusGroup fg ui
+                    coll  <- newCollection
+                    main  <- addToCollection coll ui fg
+                    _     <- exerciseList list coll main
+                    _     <- list `onItemActivated` \(ActivateItemEvent _ switch _) -> switch
+                    return coll
+                where exercisePage e coll backToMain = do ui <- plainText (exerciseStatement e)
+                                                          fg <- newFocusGroup
+                                                          fg `onKeyPressed` \_ k _ -> if k == KBS
+                                                                                         then backToMain >> return True
+                                                                                         else return False
+                                                          exitOnCtrlQ fg
+                                                          _  <- addToFocusGroup fg ui
+                                                          addToCollection coll ui fg
+                      exerciseList l coll backToMain = forM_ exercises (\e -> do title <- plainText $ exerciseTitle e
+                                                                                 page  <- exercisePage e coll backToMain
+                                                                                 addToList l page title)
+                      exitOnCtrlQ fg      = fg `onKeyPressed` \_ k modifiers -> if k == KChar 'q' && MCtrl `elem` modifiers
+                                                                                   then exitSuccess
+                                                                                   else return False
+
+
