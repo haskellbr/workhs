96010be716ce4eb5d7af8d1f8c94d07b9d1d2862
asakeron <asakeron@gmail.com>
2015-01-03 02:02:17 -0200

    Parse command line arguments.

--- src/Engager/Core.hs
+++ src/Engager/Core.hs
@@ -1,4 +1,4 @@
-module Engager.Core where
+module Engager.Core (run) where
 
 import           Data.Map.Strict                (Map)
 import qualified Data.Map.Strict                as M
@@ -7,7 +7,24 @@ import           Engager.Exercise               (Exercise)
 import           Engager.Interface              (mainScreen)
 import           Graphics.Vty.Widgets.Core      (defaultContext)
 import           Graphics.Vty.Widgets.EventLoop (runUi)
+import           Options.Applicative
+
+data Options = Options { verify :: Maybe String }
+
+options :: Parser Options
+options = Options <$> (optional $ strOption (long "verify" <> short   'v'
+                                                           <> metavar "Exercise"
+                                                           <> help    "Which exercise to verify"))
+
+start :: Map Text Exercise -> Options -> IO ()
+start exercises (Options Nothing)         = do screen <- mainScreen $ map snd $ M.toList exercises
+                                               runUi screen defaultContext
+start exercises (Options (Just exercise)) = putStrLn $ "Verify " ++ exercise
 
 run :: Map Text Exercise -> IO ()
-run exercises = do screen <- mainScreen $ map snd $ M.toList exercises
-                   runUi screen defaultContext
+run exercises = do args <- execParser opts
+                   start exercises args
+  where
+    opts = info (helper <*> options)
+                (fullDesc <> progDesc ""
+                          <> header   "")
