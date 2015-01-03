19cc4c6bd6cc34c0d835bf3fe22f1d6fa9655043
asakeron <asakeron@gmail.com>
2015-01-03 00:21:30 -0200

    run takes a Map Text Exercise instead of [Exercise]

--- src/Engager/Core.hs
+++ src/Engager/Core.hs
@@ -1,10 +1,13 @@
 module Engager.Core where
 
-import Engager.Exercise               (Exercise)
-import Engager.Interface              (mainScreen)
-import Graphics.Vty.Widgets.Core      (defaultContext)
-import Graphics.Vty.Widgets.EventLoop (runUi)
+import           Data.Map.Strict                (Map)
+import qualified Data.Map.Strict                as M
+import           Data.Text                      (Text)
+import           Engager.Exercise               (Exercise)
+import           Engager.Interface              (mainScreen)
+import           Graphics.Vty.Widgets.Core      (defaultContext)
+import           Graphics.Vty.Widgets.EventLoop (runUi)
 
-run :: [Exercise] -> IO ()
-run exercises = do screen <- mainScreen exercises
+run :: Map Text Exercise -> IO ()
+run exercises = do screen <- mainScreen $ map snd $ M.toList exercises
                    runUi screen defaultContext
