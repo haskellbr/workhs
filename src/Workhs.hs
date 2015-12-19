80f984eaba3e6b46de8a2fb10319b7ce34610406
yamadapc <tacla.yamada@gmail.com>
2015-12-19 17:29:59 -0200

    Working basic GHC orchestration

--- src/Workhs.hs
+++ src/Workhs.hs
@@ -21,20 +21,33 @@ import           Data.Text                     (Text)
 import qualified Data.Text                     as Text
 import qualified Data.Text.IO                  as Text
 import           System.Console.ListPrompt
-import           System.Environment            (getProgName)
+import           System.Directory
+import           System.Environment            (getArgs, getProgName)
+import           System.Exit
+import           System.FilePath
+import           System.Process
 
 data Task = Task { taskTitle       :: Text
                  , taskDescription :: Text
+                 , taskVerify      :: TaskVerifier
                  }
   deriving(Show)
+
+data TaskVerifier = TaskVerifierIO (FilePath -> IO Bool)
+
+instance Show TaskVerifier where
+  show (TaskVerifierIO _) = "TaskVerifierIO"
+
+
+runVerifier (TaskVerifierIO test) = test
 
 taskHelloWorldDescription = [here|
 Write a Haskell program that prints "Hello World" to the console (stdout).
 
 ## Tips
 To make a Haskell program, create a file with the `.hs` extension. Your file
-will have to define a `main` function. You can test your program with `runghc`
-or `stack runghc`. For example:
+will have to define a `main` function. You can test your program with
+`runhaskell` or `stack runhaskell`. For example:
 
     $ runghc MyProgram.hs
 
@@ -45,11 +58,27 @@ You can print things to the terminal using `putStrLn`:
 
 defaultTasks :: [Task]
 defaultTasks = [ Task "Hello World!" taskHelloWorldDescription
-               , Task "First functions" ""
-               , Task "Introduction to lists" ""
-               , Task "List comprehensions" ""
-               , Task "Tuples" ""
+                     (TaskVerifierIO (verifyOutput "Hello World"))
+               , Task "First functions" "" undefined
+               , Task "Introduction to lists" "" undefined
+               , Task "List comprehensions" "" undefined
+               , Task "Tuples" "" undefined
                ]
+
+verifyOutput :: Text -> FilePath -> IO Bool
+verifyOutput out fp = do
+    tmp <- getTemporaryDirectory
+    print tmp
+    e <- spawnCommand
+             ("stack ghc -- -o " <> (tmp </> "workhs-verify") <> " " <> fp) >>=
+         waitForProcess
+    case e of
+        ExitFailure _ -> error "Failed to compile"
+        ExitSuccess -> do
+            putStrLn (tmp </> "workhs-verify")
+            (_, o, _) <- readProcessWithExitCode (tmp </> "workhs-verify") [] ""
+            print o
+            return True
 
 data Options = Options { title       :: Text
                        , description :: Text
@@ -73,13 +102,24 @@ footer prog = Text.unlines [ "When you're finished with your code, type:"
 
 defaultMain :: Options -> IO ()
 defaultMain Options{..} = do
-    prog <- getProgName
-    -- TODO list-prompt should use Text
-    taskt <- Text.pack <$> simpleListPrompt def (map (Text.unpack . taskTitle) tasks)
-    let Just task = find ((== taskt) . taskTitle) tasks
-        md = Text.unlines [ "# " <> title
-                          , "## " <> taskTitle task
-                          , taskDescription task
-                          , footer prog
-                          ]
-    prettyPrint (markdown def md)
+    as <- getArgs
+    case as of
+        "verify":fp:_ -> do
+            -- TODO read state from $HOME/.config
+            let currentTask = head tasks
+            valid <- runVerifier (taskVerify currentTask) fp
+            if valid
+                then putStrLn "Bazinga!"
+                else putStrLn "Ops..."
+        [] -> do
+            prog <- getProgName
+            -- TODO list-prompt should use Text
+            taskt <- Text.pack <$> simpleListPrompt def (map (Text.unpack . taskTitle) tasks)
+            let Just task = find ((== taskt) . taskTitle) tasks
+                md = Text.unlines [ "# " <> title
+                                  , "## " <> taskTitle task
+                                  , taskDescription task
+                                  , footer prog
+                                  ]
+            prettyPrint (markdown def md)
+        _ -> error "Failed to parse arguments"
