f84e81b8f620942ae7b629911fe29fb8c9148588
yamadapc <tacla.yamada@gmail.com>
2015-12-18 23:14:06 -0200

    Something we can compile and run

--- src/Workhs.hs
+++ src/Workhs.hs
@@ -13,13 +13,17 @@ import           Data.Text                 (Text)
 import qualified Data.Text.IO              as Text
 import           System.Console.ListPrompt
 
+data Task = Task String
+
 data Options = Options { title       :: Text
                        , description :: Text
+                       , tasks       :: [Task]
                        }
 
 instance Default Options where
     def = Options { title = "My Tutorial"
-                  , description = "# You'll be challenged to complete this!\n"
+                  , description = "# You'll be challenged to complete this!"
+                  , tasks = []
                   }
 
 defaultMain :: Options -> IO ()
